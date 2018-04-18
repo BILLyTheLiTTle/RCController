d029d15702ce8b34b32499f798d8ab9a4217e0f3
Vasilios Tsapalos <littleprog@gmail.com>
2018-04-18 13:58:46 +0300

    Placeholders for feedback info

--- app/src/main/java/car/rccontroller/RCControllerActivity.kt
+++ app/src/main/java/car/rccontroller/RCControllerActivity.kt
@@ -18,13 +18,13 @@ class RCControllerActivity : AppCompatActivity() {
         // Note that some of these constants are new as of API 16 (Jelly Bean)
         // and API 19 (KitKat). It is safe to use them, as they are inlined
         // at compile-time and do nothing on earlier devices.
-        fullscreen_content.systemUiVisibility =
-                View.SYSTEM_UI_FLAG_LOW_PROFILE or
-                View.SYSTEM_UI_FLAG_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        //fullscreen_content.systemUiVisibility =
+                //View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                //View.SYSTEM_UI_FLAG_FULLSCREEN or
+                //View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                //View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                //View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                //View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
     }
     private val mShowPart2Runnable = Runnable {
         // Delayed display of UI elements
@@ -71,9 +71,9 @@ class RCControllerActivity : AppCompatActivity() {
 
     private fun show() {
         // Show the system bar
-        fullscreen_content.systemUiVisibility =
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        //fullscreen_content.systemUiVisibility =
+                //View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                //View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
         mVisible = true
 
         // Schedule a runnable to display UI elements after a delay
