2e81b172b1d38d96b320ac2f723a942e58391f66
Vasilios Tsapalos <littleprog@gmail.com>
2018-04-19 10:48:18 +0300

    Clear some things

--- app/src/main/java/car/rccontroller/RCControllerActivity.kt
+++ app/src/main/java/car/rccontroller/RCControllerActivity.kt
@@ -2,100 +2,22 @@ package car.rccontroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 class RCControllerActivity : AppCompatActivity() {
-    private val mHideHandler = Handler()
-    private val mHidePart2Runnable = Runnable {
-        // Delayed removal of status and navigation bar
-
-        // Note that some of these constants are new as of API 16 (Jelly Bean)
-        // and API 19 (KitKat). It is safe to use them, as they are inlined
-        // at compile-time and do nothing on earlier devices.
-        //fullscreen_content.systemUiVisibility =
-                //View.SYSTEM_UI_FLAG_LOW_PROFILE or
-                //View.SYSTEM_UI_FLAG_FULLSCREEN or
-                //View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                //View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                //View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                //View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-    }
-    private val mShowPart2Runnable = Runnable {
-        // Delayed display of UI elements
-        supportActionBar?.show()
-        //fullscreen_content_controls.visibility = View.VISIBLE
-    }
-    private var mVisible: Boolean = false
-    private val mHideRunnable = Runnable { hide() }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_rccontroller)
+
         supportActionBar?.setDisplayHomeAsUpEnabled(false)
-
-        mVisible = true
-        hide()
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        //dummy_button.setOnTouchListener(mDelayHideTouchListener)
-    }
-
-    private fun toggle() {
-        if (mVisible) {
-            hide()
-        } else {
-            show()
-        }
-    }
-
-    private fun hide() {
-        // Hide UI first
         supportActionBar?.hide()
-        //fullscreen_content_controls.visibility = View.GONE
-        mVisible = false
 
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable)
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY.toLong())
     }
 
-    private fun show() {
-        // Show the system bar
-        //fullscreen_content.systemUiVisibility =
-                //View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                //View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        mVisible = true
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable)
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY.toLong())
-    }
-
-    companion object {
-        /**
-         * Whether or not the system UI should be auto-hidden after
-         * [AUTO_HIDE_DELAY_MILLIS] milliseconds.
-         */
-        private val AUTO_HIDE = true
-
-        /**
-         * If [AUTO_HIDE] is set, the number of milliseconds to wait after
-         * user interaction before hiding the system UI.
-         */
-        private val AUTO_HIDE_DELAY_MILLIS = 3000
-
-        /**
-         * Some older devices needs a small delay between UI widget updates
-         * and a change of the status and navigation bar.
-         */
-        private val UI_ANIMATION_DELAY = 300
-    }
 }
