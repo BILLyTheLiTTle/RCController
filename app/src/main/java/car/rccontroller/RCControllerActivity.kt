357bdd8b8d22b80d5064cfe7cbf591294d616306
Vasilios Tsapalos <littleprog@gmail.com>
2018-04-19 11:14:39 +0300

    Set default values for the seekbars

--- app/src/main/java/car/rccontroller/RCControllerActivity.kt
+++ app/src/main/java/car/rccontroller/RCControllerActivity.kt
@@ -2,6 +2,8 @@ package car.rccontroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_rccontroller.*
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -18,6 +20,17 @@ class RCControllerActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(false)
         supportActionBar?.hide()
 
+        steering_seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onStopTrackingTouch(seekBar: SeekBar) { seekBar.progress = 50 }
+            override fun onStartTrackingTouch(seekBar: SeekBar){}
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean){}
+        })
+
+        throttleNbrake_mySeekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onStopTrackingTouch(seekBar: SeekBar) { seekBar.progress = 50 }
+            override fun onStartTrackingTouch(seekBar: SeekBar){}
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean){}
+        })
     }
 
 }
