db6855473881153275bd95bee0499495c85b8334
Vasilios Tsapalos <littleprog@gmail.com>
2018-04-19 10:44:05 +0300

    From LinearLayout to ConstraintLayout

--- app/src/main/java/car/rccontroller/RCControllerActivity.kt
+++ app/src/main/java/car/rccontroller/RCControllerActivity.kt
@@ -3,8 +3,6 @@ package car.rccontroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.view.View
-import kotlinx.android.synthetic.main.activity_rccontroller.*
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
